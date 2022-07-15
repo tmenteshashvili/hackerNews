fb6b7afc93e76bb5d304192f13cde6bdeff78ee9
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2022-07-15 13:32:25 +0400

    Initial Commit

---
+++ hackerNews/hackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackerNewsApp.swift
+//  hackerNews
+//
+//  Created by Tako Menteshashvili on 15.07.22.
+//
+
+import SwiftUI
+
+@main
+struct hackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
